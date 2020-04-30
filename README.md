645c206cc38637b0835c8e3fa5afd250f36cdd8a
Christian Robinson <chrisdrobbo@gmail.com>
2020-04-30 10:30:18 +0100

    Created JS, HTML, Stylesheet files. Also updated README.md Added a JavaScript function to create the 16x16 grid

---
+++ README.md
@@ -0,0 +1,18 @@
+# The Odin Project - Web Development 101
+
+## Project - Etch-A-Sketch
+
+This project was to create an Etch-A-Sketch / sketchpad web app in order to solidify my knowledge of using HTML and CSS and specifically JavaScript, which I am using for DOM manipulation. 
+
+## Summary 
+
+(add a description of what I did)
+
+## Key Results
+
+(add key results)
+
+
+## Improvements  
+
+(add improvements)
---
+++ javascript.js
@@ -0,0 +1,19 @@
+const sketchArea = document.getElementById('sketch-area');
+
+// const gridSqaure = document.createElement('div');
+// // gridSqaure.setAttribute = ('id', 'grid-square');
+// gridSqaure.classList.add = ('grid-square');
+// sketchArea.appendChild(gridSqaure);
+
+
+function makeRows(rows, cols) {
+    sketchArea.style.setProperty('--grid-rows', rows);
+    sketchArea.style.setProperty('--grid-cols', cols);
+    for (c = 0; c < (rows*cols); c++) {
+        let cell = document.createElement('div');
+        cell.innerText = (c + 1);
+        sketchArea.appendChild(cell).className = "grid-item";
+    }
+}
+
+makeRows(16, 16);
